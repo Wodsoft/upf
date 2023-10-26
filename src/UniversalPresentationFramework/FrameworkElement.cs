41b2586c65282e001c474f8f9e23cf7d555e20d1
Kation <kation@outlook.com>
2023-10-26 23:21:36 +0800

    逻辑树

--- src/UniversalPresentationFramework/FrameworkElement.cs
+++ src/UniversalPresentationFramework/FrameworkElement.cs
@@ -8,5 +8,39 @@ namespace Wodsoft.UI
 {
     public class FrameworkElement : UIElement
     {
+        #region Logical
+
+        public FrameworkElement? Parent { get; private set; }
+
+        protected override DependencyObject? GetInheritanceParent()
+        {
+            return Parent;
+        }
+
+        protected internal void AddLogicalChild(FrameworkElement child)
+        {
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+            if (child.Parent != null)
+                throw new InvalidOperationException("Child has its parent already.");
+            child.Parent = this;
+        }
+
+        protected internal void RemoveLogicalChild(FrameworkElement child)
+        {
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+            if (child.Parent == null)
+                throw new InvalidOperationException("Child has no parent.");
+            if (child.Parent != this)
+                throw new InvalidOperationException("This element is not the parent of child.");
+            child.Parent = null;
+        }
+
+        #endregion
+
+        #region Layout
+
+        #endregion
     }
 }
