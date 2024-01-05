cde5ea567417a8bbb3d312a654bba082f16ade4e
Kation <kation@outlook.com>
2024-01-05 20:43:47 +0800

    逻辑根改变事件

---
+++ src/UniversalPresentationFramework.Abstractions/LogicalRootChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wodsoft.UI
+{
+    public class LogicalRootChangedEventArgs : EventArgs
+    {
+        public LogicalRootChangedEventArgs(LogicalObject oldRoot, LogicalObject newRoot)
+        {
+            OldRoot = oldRoot;
+            NewRoot = newRoot;
+        }
+
+        public LogicalObject OldRoot { get; }
+
+        public LogicalObject NewRoot { get; }
+    }
+}
