e649048f96d6529add2622500e52bb52787178bf
Kation <kation@outlook.com>
2024-03-11 22:36:21 +0800

    禁止并发测试

--- test/UniversalPersentationFramework.BuildTest/SourceGeneratorTest.cs
+++ test/UniversalPersentationFramework.BuildTest/SourceGeneratorTest.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UniversalPresentationFramework.SourceGenerators;
 
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 namespace UniversalPersentationFramework.BuildTest
 {
     public class SourceGeneratorTest
