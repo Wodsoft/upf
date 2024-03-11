ad0336ff8f077530b64981fb9be0a530c8be99e7
Kation <kation@outlook.com>
2024-03-11 22:40:03 +0800

    Control增加外观属性

--- src/UniversalPresentationFramework/Controls/Control.cs
+++ src/UniversalPresentationFramework/Controls/Control.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xaml;
 using System.Xaml.Markup;
+using Wodsoft.UI.Media;
 
 namespace Wodsoft.UI.Controls
 {
@@ -15,6 +17,29 @@ namespace Wodsoft.UI.Controls
         private ControlTemplate? _template;
 
         protected override FrameworkTemplate? GetTemplate() => _template;
+
+        public static readonly DependencyProperty TemplateProperty =
+            DependencyProperty.Register(
+                "Template",
+                typeof(ControlTemplate),
+                typeof(Control),
+                new FrameworkPropertyMetadata(
+                        null,  // default value
+                        FrameworkPropertyMetadataOptions.AffectsMeasure,
+                        new PropertyChangedCallback(OnTemplateChanged)));
+        private static void OnTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Control c = (Control)d;
+            c._template = (ControlTemplate?)e.NewValue;
+            c.OnTemplateChanged();
+            //StyleHelper.UpdateTemplateCache(c, (FrameworkTemplate)e.OldValue, (FrameworkTemplate)e.NewValue, TemplateProperty);
+        }
+
+        public ControlTemplate? Template
+        {
+            get { return _template; }
+            set { SetValue(TemplateProperty, value); }
+        }
 
         #endregion
 
@@ -64,27 +89,84 @@ namespace Wodsoft.UI.Controls
 
         #region Properties
 
-        public static readonly DependencyProperty TemplateProperty =
-            DependencyProperty.Register(
-                "Template",
-                typeof(ControlTemplate),
-                typeof(Control),
-                new FrameworkPropertyMetadata(
-                        null,  // default value
-                        FrameworkPropertyMetadataOptions.AffectsMeasure,
-                        new PropertyChangedCallback(OnTemplateChanged)));
-        private static void OnTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        public static readonly DependencyProperty BorderBrushProperty
+                = Border.BorderBrushProperty.AddOwner(typeof(Control),
+                    new FrameworkPropertyMetadata(
+                        Border.BorderBrushProperty.DefaultMetadata.DefaultValue,
+                        FrameworkPropertyMetadataOptions.None));
+        public Brush? BorderBrush
         {
-            Control c = (Control)d;
-            c._template = (ControlTemplate?)e.NewValue;
-            c.OnTemplateChanged();
-            //StyleHelper.UpdateTemplateCache(c, (FrameworkTemplate)e.OldValue, (FrameworkTemplate)e.NewValue, TemplateProperty);
+            get { return (Brush?)GetValue(BorderBrushProperty); }
+            set { SetValue(BorderBrushProperty, value); }
         }
 
-        public ControlTemplate? Template
+        public static readonly DependencyProperty BorderThicknessProperty
+                = Border.BorderThicknessProperty.AddOwner(typeof(Control),
+                    new FrameworkPropertyMetadata(
+                        Border.BorderThicknessProperty.DefaultMetadata.DefaultValue,
+                        FrameworkPropertyMetadataOptions.None));
+        public Thickness BorderThickness
         {
-            get { return _template; }
-            set { SetValue(TemplateProperty, value); }
+            get { return (Thickness)GetValue(BorderThicknessProperty)!; }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundProperty =
+                Panel.BackgroundProperty.AddOwner(typeof(Control),
+                    new FrameworkPropertyMetadata(
+                        Panel.BackgroundProperty.DefaultMetadata.DefaultValue,
+                        FrameworkPropertyMetadataOptions.None));
+        public Brush? Background
+        {
+            get { return (Brush?)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        //public static readonly DependencyProperty ForegroundProperty =
+        //        TextElement.ForegroundProperty.AddOwner(
+        //                typeof(Control),
+        //                new FrameworkPropertyMetadata(SystemColors.ControlTextBrush,
+        //                    FrameworkPropertyMetadataOptions.Inherits));
+        //public Brush? Foreground
+        //{
+        //    get { return (Brush?)GetValue(ForegroundProperty); }
+        //    set { SetValue(ForegroundProperty, value); }
+        //}
+
+        public static readonly DependencyProperty HorizontalContentAlignmentProperty =
+                    DependencyProperty.Register(
+                                "HorizontalContentAlignment",
+                                typeof(HorizontalAlignment),
+                                typeof(Control),
+                                new FrameworkPropertyMetadata(HorizontalAlignment.Left));
+        public HorizontalAlignment HorizontalContentAlignment
+        {
+            get { return (HorizontalAlignment)GetValue(HorizontalContentAlignmentProperty)!; }
+            set { SetValue(HorizontalContentAlignmentProperty, value); }
+        }
+
+        public static readonly DependencyProperty VerticalContentAlignmentProperty =
+                    DependencyProperty.Register(
+                                "VerticalContentAlignment",
+                                typeof(VerticalAlignment),
+                                typeof(Control),
+                                new FrameworkPropertyMetadata(VerticalAlignment.Top));
+        public VerticalAlignment VerticalContentAlignment
+        {
+            get { return (VerticalAlignment)GetValue(VerticalContentAlignmentProperty)!; }
+            set { SetValue(VerticalContentAlignmentProperty, value); }
+        }
+
+        public static readonly DependencyProperty PaddingProperty
+            = DependencyProperty.Register("Padding",
+                                        typeof(Thickness), typeof(Control),
+                                        new FrameworkPropertyMetadata(
+                                                new Thickness(),
+                                                FrameworkPropertyMetadataOptions.AffectsParentMeasure));
+        public Thickness Padding
+        {
+            get { return (Thickness)GetValue(PaddingProperty)!; }
+            set { SetValue(PaddingProperty, value); }
         }
 
         #endregion
