b1de862be3f31a1ce6c519d429e49deb7b5b32fe
Kation <kation@outlook.com>
2024-05-13 22:33:41 +0800

    修复命名

--- src/UniversalPresentationFramework.Core/Assembly.cs
+++ src/UniversalPresentationFramework.Core/Assembly.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Xaml.Markup;
 
 [assembly: InternalsVisibleTo("UniversalPresentationFramework")]
-[assembly: InternalsVisibleTo("UniversalPersentationFramework.FrameworkTest")]
+[assembly: InternalsVisibleTo("UniversalPresentationFramework.FrameworkTest")]
 [assembly: InternalsVisibleTo("UniversalPresentationFramework.SkiaRenderTest")]
 [assembly: XmlnsDefinition("http://schemas.wodsoft.com/upf/presentation", "Wodsoft.UI")]
 [assembly: XmlnsDefinition("http://schemas.wodsoft.com/upf/presentation", "Wodsoft.UI.Media")]
