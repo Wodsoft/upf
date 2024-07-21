f890a762b1ba235e92a4421b5f3b6bb3f7f8bbc0
Kation <kation@outlook.com>
2024-07-21 18:37:53 +0800

    ReadOnlyFrameworkPropertyMetadata

---
+++ src/UniversalPresentationFramework/ReadOnlyFrameworkPropertyMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wodsoft.UI
+{
+    public class ReadOnlyFrameworkPropertyMetadata : FrameworkPropertyMetadata
+    {
+        private GetReadOnlyValueCallback _getValueCallback;
+
+        public ReadOnlyFrameworkPropertyMetadata(object defaultValue, GetReadOnlyValueCallback getValueCallback) :
+            base(defaultValue)
+        {
+            _getValueCallback = getValueCallback;
+        }
+
+        public override GetReadOnlyValueCallback? GetReadOnlyValueCallback => _getValueCallback;
+    }
+}
