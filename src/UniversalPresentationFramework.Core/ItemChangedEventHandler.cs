835b1406b7990458e7651ee879012e1a27b4ebb3
Kation <kation@outlook.com>
2024-07-23 21:47:38 +0800

    ItemChangedEventHandler

---
+++ src/UniversalPresentationFramework.Core/ItemChangedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wodsoft.UI
+{
+    public delegate void ItemChangedEventHandler<T>(object sender, T item);
+}
